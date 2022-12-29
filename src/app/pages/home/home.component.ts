98c874694a81d62f23ae11490f51c22891500d8e
Leonel Huachaca <leonel.huachacav@gmail.com>
2022-12-29 11:15:41 -0500

    add functions lanzar and numeroRandom

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  dado: number = 0;
+  lista: number[] = [1, 2, 3, 4, 5, 6];
+  prevUsado: number = 0;
+  lanzar() {
+    const numero = this.numeroRandom();
+    if (numero === this.prevUsado) {
+      this.dado = this.numeroRandom();
+    }
+    this.prevUsado = numero;
+    this.dado = numero;
+  }
+  numeroRandom(): number {
+    return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  }
 }
